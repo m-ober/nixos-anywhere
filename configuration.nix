8e625ab6d367194ac0da78727740eb66ac6701a1
Micha Ober <git@ober-mail.de>
2025-07-19 17:09:13 +0200

    testing strichliste module

--- configuration.nix
+++ configuration.nix
@@ -3,13 +3,19 @@
   lib,
   pkgs,
   ...
-} @ args:
+}@args:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -18,16 +24,25 @@
   };
   services.openssh.enable = true;
 
+  services.qemuGuest.enable = true;
+
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
+    pkgs.nginx
+    pkgs.local.strichliste-frontend
+    pkgs.local.strichliste-backend
   ];
 
-  users.users.root.openssh.authorizedKeys.keys =
-  [
+  services.strichliste = {
+    enable = true;
+    domain = "strichliste.micha.gg";
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
-    "# CHANGE"
-  ] ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMH5HzYU7kMeWjYOe7bbRls/uEefNEAJAz3NwTwKQBy9 mober@Michas-MBA.local"
+  ] ++ (args.extraPublicKeys or [ ]); # this is used for unit-testing this module and can be removed if not needed
 
   system.stateVersion = "24.05";
 }
--- flake.nix
+++ flake.nix
@@ -1,69 +1,29 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-local.url = "path:/Users/mober/projects/nixpkgs";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
-  outputs =
-    {
-      nixpkgs,
-      disko,
-      nixos-facter-modules,
-      ...
-    }:
-    {
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
+  outputs = { nixpkgs, nixpkgs-local, disko, ... }:
+    let
+      system = "x86_64-linux";
+      overlay-local = final: prev: {
+        local = nixpkgs-local.legacyPackages.${prev.system};
       };
-      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
-      nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./digitalocean.nix
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          ./configuration.nix
-        ];
-      };
-      nixosConfigurations.hetzner-cloud-aarch64 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
-
-      # Use this for all other targets
-      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
+      localModulesPath = nixpkgs-local + "/nixos/modules";
+    in {
       nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-local ]; })
+
+          (import (localModulesPath + "/services/web-apps/strichliste.nix"))
+
           disko.nixosModules.disko
           ./configuration.nix
           ./hardware-configuration.nix
         ];
       };
-
-      # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
-      # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
-      nixosConfigurations.generic-nixos-facter = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          nixos-facter-modules.nixosModules.facter
-          {
-            config.facter.reportPath =
-              if builtins.pathExists ./facter.json then
-                ./facter.json
-              else
-                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
-          }
-        ];
-      };
     };
+
 }
